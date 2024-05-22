1feb515881c43405d46e420f8e8fc024ff6fb907
Alex Reyder <razrab@bk.ru>
2024-05-22 17:09:26 +0300

    Hi it new me!

--- public/api/auth/logout.php
+++
@@ -1,3 +0,0 @@
-<?
-setcookie("id", "", time() - 3600 * 24 * 30 * 12, "/");
-setcookie("hash", "", time() - 3600 * 24 * 30 * 12, "/", null, null, true);
--- public/api/savemap.php
+++
@@ -1,3 +0,0 @@
-<?php
-$rootDir = realpath($_SERVER["DOCUMENT_ROOT"]);
-file_put_contents($rootDir . '/assets/data/map.json', $_POST['data']);
